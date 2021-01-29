d422dd0901d9945bd14ef0701c3afcdd7c7f911d
Miguel Soteldo <74800042+STLDOv2@users.noreply.github.com>
2021-01-29 15:05:55 -0600

    Add files via upload

---
+++ WordCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Word_Count
+{
+    class WordCount
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a sentence: ");
+
+            string inputSentence = Console.ReadLine();
+
+            Console.WriteLine(WordCounter(inputSentence));
+
+            Console.ReadLine();
+
+        }
+        static string WordCounter(string sentence)
+        {
+            int wordCount = sentence.Split(' ').Length;
+            return $"The word count is {Convert.ToString(wordCount)}";
+        }
+    }
+}
